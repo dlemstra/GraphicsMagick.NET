1a093ad45c4c98c17c79eba3b673377030d8ea8d
dlemstra <dirk@git.imagemagick.org>
2014-07-23 22:03:08 -0700

    Fixed bug in ExifProfile.

--- GraphicsMagick.NET/Profiles/Exif/ExifProfile.h
+++ GraphicsMagick.NET/Profiles/Exif/ExifProfile.h
@@ -47,9 +47,9 @@ namespace GraphicsMagick
 	public:
 		///==========================================================================================
 		///<summary>
-		/// Initializes a new instance of the IptcProfile class.
+		/// Initializes a new instance of the ExifProfile class.
 		///</summary>
-		ExifProfile() : ImageProfile("iptc") {};
+		ExifProfile() : ImageProfile("exif") {};
 		///==========================================================================================
 		///<summary>
 		/// Initializes a new instance of the ExifProfile class.
