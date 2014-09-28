ab798f722fa399cdf78d6bc7b3f068f5ccf9a914
dlemstra <dirk@git.imagemagick.org>
2014-09-28 03:03:18 -0700

    Published GraphicsMagick.NET 1.3.20.2.

--- Publish/Publish.cmd
+++ Publish/Publish.cmd
@@ -1,4 +1,4 @@
 @echo off
 call "%vs110comntools%vsvars32.bat"
-powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "1.3.20" "1.3.20.1"
+powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "1.3.20" "1.3.20.2"
 pause
