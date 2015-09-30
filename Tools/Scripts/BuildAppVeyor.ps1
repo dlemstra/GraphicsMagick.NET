f9867b9a81ed754b9435b8ce4585478cc97b2f68
dlemstra <dirk@git.imagemagick.org>
2015-09-30 09:38:01 +0200

    Added AnyCPU build to appveyor.yml.

--- Tools/Scripts/BuildAppVeyor.ps1
+++ Tools/Scripts/BuildAppVeyor.ps1
@@ -3,6 +3,8 @@ $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Path
 . $scriptPath\Shared\Functions.ps1
 SetFolder $scriptPath
 #==================================================================================================
+. Tools\Scripts\Shared\GzipAssembly.ps1
+#==================================================================================================
 function GetConfig($platform, $name)
 {
   $config = $null
@@ -35,13 +37,37 @@ function GetConfig($platform, $name)
       $config.config = "Tests"
       $config.platformName = "Win32"
     }
+    elseif ($platform -eq "AnyCPU")
+    {
+      $config.config = "Tests"
+      $config.solution = "GraphicsMagick.NET.AnyCPU.sln"
+    }
   }
 
   return $config
 }
 #==================================================================================================
-function BuildConfiguration($config)
+function BuildConfiguration($config, $name)
 {
+  if ($config.platform -eq "AnyCPU")
+  {
+    $config_x86 = GetConfig "x86" $name
+    $config_x86.config = "Release"
+    BuildConfiguration($config_x86)
+
+    $config_x64 = GetConfig "x64" $name
+    BuildConfiguration($config_x64)
+
+    if ($config.quantum -eq "Q8")
+    {
+      GzipAssembliesQ8
+    }
+    else
+    {
+      GzipAssembliesQ16
+    }
+  }
+
   $properties = "Configuration=$($config.config)$($config.quantum),Platform=$($config.platformName)"
   BuildSolution $config.solution $properties
 }
@@ -56,4 +82,4 @@ if ($config -eq $null)
   throw "Unknown configuration: $name"
 }
 
-BuildConfiguration $config
+BuildConfiguration $config $name
