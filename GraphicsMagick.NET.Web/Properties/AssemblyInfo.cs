a1b99165b1d1a26f1e876e3dff498f882f7d5590
Dirk Lemstra <dirk@git.imagemagick.org>
2017-04-24 20:05:24 +0000

    Published GraphicsMagick.NET 1.3.25.1

--- GraphicsMagick.NET.Web/Properties/AssemblyInfo.cs
+++ GraphicsMagick.NET.Web/Properties/AssemblyInfo.cs
@@ -42,7 +42,7 @@ using System.Web;
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.25.0")]
+[assembly: AssemblyFileVersion("1.3.25.1")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 //=================================================================================================
--- Publish/Publish.cmd
+++ Publish/Publish.cmd
@@ -1,4 +1,4 @@
 @echo off
 call "%vs110comntools%vsvars32.bat"
-powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "1.3.25.0"
+powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "1.3.25.1"
 pause
